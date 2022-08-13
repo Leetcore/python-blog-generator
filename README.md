853f068c3845b066462b8ede33e5942fadc49978
leetcore <leetcore@live.de>
2022-08-13 10:05:24 +0200

    bugfixing and config improvements

--- README.md
+++ README.md
@@ -3,8 +3,9 @@ Dieses Script generiert eine Startseite mit Blogliste und baut Links zu den
 Blogeinträgen in `/blog/`. Dort sollte ein `#date` als Datum vorhanden sein.
 
 ## Template
-Der Platzhalter `[#BLOG_INDEX#]` im `/template/template.html` generiert eine
-HTML-Liste mit den Links zu den Einträgen.
+Der Platzhalter `[#BLOG_INDEX#]` im `/template/index.html` generiert eine
+HTML-Liste mit den Links zu den Einträgen. In `/template/index.html` ist die 
+Vorlage für neue Blogposts.
 
 ## Installation
 ``` cmd
@@ -18,3 +19,6 @@ Blog URL und die Blogbeschreibung kann in der `config.toml` geändert werden.
 ``` cmd
 python3 generate-blog.py
 ```
+
+## Upload
+Der Inhalt des Public-Ordner muss hochgeladen werden.
--- generate-blog.py
+++ generate-blog.py
@@ -7,7 +7,10 @@ import bs4
 template_index_filename = "template/index.html"
 template_post_filename = "template/post.html"
 
-#generate new post based on template
+with open("config.toml", "r") as config:
+    parsed_config = toml.loads(config.read())
+
+# generate new post based on template
 print("Filename:")
 new_filename = input()
 new_filename = new_filename.strip().lower()
@@ -24,7 +27,7 @@ if len(new_filename) > 0:
     today = timestamp.strftime("%d.%m.%Y")
 
     with open(template_post_filename, "r") as template:
-        with open(f"public/blog/{new_filename}", "w") as new_file:
+        with open(f"{parsed_config['public_folder']}{parsed_config['post_folder']}/{new_filename}", "w") as new_file:
             template_content = template.read()
             template_content = template_content.replace("[#HEADLINE#]", new_headline)
             template_content = template_content.replace("[#DATE#]", today)
@@ -39,71 +42,71 @@ rss_output.append('<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">'
 rss_output.append('<channel>')
 
 # read config and read blog posts
-with open("config.toml", "r") as config:
-    parsed_config = toml.loads(config.read())
-    rss_output.append(f'<title>{parsed_config["title"]}</title>')
-    rss_output.append(f'<link>{parsed_config["link"]}</link>')
-    rss_output.append(f'<description>{parsed_config["description"]}</description>')
 
-    blog_list: list[str] = []
+    
+rss_output.append(f'<title>{parsed_config["title"]}</title>')
+rss_output.append(f'<link>{parsed_config["link"]}</link>')
+rss_output.append(f'<description>{parsed_config["description"]}</description>')
 
-    for filename in glob.glob("public/blog/*.html", recursive=True):
-        with open(filename) as f:
-            # default values
-            headline = ""
-            timestamp = datetime.now()
-            description = ""
+blog_list: list[str] = []
 
-            # parse html content
-            content = f.read()
-            soup = bs4.BeautifulSoup(content, "html.parser")
+for filename in glob.glob(f"{parsed_config['public_folder']}{parsed_config['post_folder']}/*.html", recursive=True):
+    with open(filename) as f:
+        # default values
+        headline = ""
+        timestamp = datetime.now()
+        description = ""
 
-            # read and format date
-            date_tag_content = soup.find(id="date")
-            if date_tag_content:
-                date_string = re.search(r"\d\d\.\d\d\.\d\d\d\d", date_tag_content.text)
-                if date_string:
-                    timestamp = datetime.strptime(date_string.group(), "%d.%m.%Y")
-            pubdate = timestamp.strftime("%a, %d %b %Y %H:%M:%S +0100")
+        # parse html content
+        content = f.read()
+        soup = bs4.BeautifulSoup(content, "html.parser")
 
-            # find headline
-            headline_tag = soup.find("h1")
-            if headline_tag:
-                headline = headline_tag.text
-            print(headline)
+        # read and format date
+        date_tag_content = soup.find(id="date")
+        if date_tag_content:
+            date_string = re.search(r"\d\d\.\d\d\.\d\d\d\d", date_tag_content.text)
+            if date_string:
+                timestamp = datetime.strptime(date_string.group(), "%d.%m.%Y")
+        pubdate = timestamp.strftime("%a, %d %b %Y %H:%M:%S +0100")
 
-            # find description
-            description_tag = soup.find("p")
-            if description_tag:
-                description = description_tag.text
-            
-            # build link
-            relative_filename = filename.replace("public", "")
-            link = relative_filename
-            guid = relative_filename
+        # find headline
+        headline_tag = soup.find("h1")
+        if headline_tag:
+            headline = headline_tag.text
+        print(headline)
 
-            rss_output.append(f'<atom:link href="{parsed_config["link"]}/blog/feed.rss" rel="self" type="application/rss+xml" />')
+        # find description
+        description_tag = soup.find("p")
+        if description_tag:
+            description = description_tag.text
+        
+        # build link
+        relative_filename = filename.replace(parsed_config['public_folder'], "")
+        link = relative_filename
+        guid = relative_filename
 
-            # build items for rss feed
-            rss_output.append("<item>")
-            rss_output.append(f"<title>{headline}</title>")
-            rss_output.append(f"<description>{description}</description>")
-            rss_output.append(f'<link>{parsed_config["link"]}{link}</link>')
-            rss_output.append(f'<guid>{parsed_config["link"]}{guid}</guid>')
-            rss_output.append(f'<pubDate>{pubdate}</pubDate>')
-            rss_output.append("</item>")
+        rss_output.append(f'<atom:link href="{parsed_config["link"]}{parsed_config["post_folder"]}/feed.rss" rel="self" type="application/rss+xml" />')
 
-            # build html elements for index
-            element_list.append('<li>')
-            element_list.append(f'<a href="{link}">{headline}</a>')
-            element_list.append(f'<p>{description}</p>')
-            element_list.append('</li>')
+        # build items for rss feed
+        rss_output.append("<item>")
+        rss_output.append(f"<title>{headline}</title>")
+        rss_output.append(f"<description>{description}</description>")
+        rss_output.append(f'<link>{parsed_config["link"]}{link}</link>')
+        rss_output.append(f'<guid>{parsed_config["link"]}{guid}</guid>')
+        rss_output.append(f'<pubDate>{pubdate}</pubDate>')
+        rss_output.append("</item>")
+
+        # build html elements for index
+        element_list.append('<li>')
+        element_list.append(f'<a href="{link}">{headline}</a>')
+        element_list.append(f'<p>{description}</p>')
+        element_list.append('</li>')
 
 rss_output.append('</channel>\n</rss>')
 element_list.append("</ul>")
 
 # write rss file
-with open("public/blog/feed.rss", "w") as f:
+with open(f"{parsed_config['public_folder']}{parsed_config['post_folder']}/feed.rss", "w") as f:
     print("Write feed.rss...")
     f.write("\n".join(rss_output))
 
@@ -115,6 +118,6 @@ with open(template_index_filename, "r") as f:
     new_content = content.replace("[#BLOG_INDEX#]", "\n".join(element_list))
 
 # write to file
-with open("public/index.html", "w") as f:
+with open(f"{parsed_config['public_folder']}/index.html", "w") as f:
     print("Write index.html...")
     f.write(new_content)
